0ccbbcc50bde5c88f1a110492485cd2d742c6ba8
Nikola Milikic <nikola.milikic@gmail.com>
2018-10-24 14:55:09 +0200

    Update package names in the 'test' folder.

--- test/test/PokreniTestove.java
+++ test/test/PokreniTestove.java
@@ -10,9 +10,9 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
-import test.zadatak1.KucniAparatTest;
-import test.zadatak1.TelevizorTest;
-import test.zadatak2.RadioTest;
+import zadatak1.KucniAparatTest;
+import zadatak1.TelevizorTest;
+import zadatak2.RadioTest;
 
 /**
  * This class executes all tests and prints out the report.
